c51cc37f94f792fd86268255b5b6e016eb500d92
Allan Cassio <allan.cassio@oqvocequer.com.br>
2022-03-26 15:20:07 -0300

    concluido primeiro pojeto apiFin

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const customers = []
 const verifyIfExistsAccountCPF = (req, res, next) => {
   const { cpf } = req.headers
 
-  const customer = customers.find(curtomer => curtomer.cpf === cpf)
+  const customer = customers.find(customer => customer.cpf === cpf)
 
   if(!customer) return res.status(400).json({error: "Cliente não encontrado."})
 
@@ -96,6 +96,49 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
   customer.statement.push(statementOperation)
 
   return res.status(201). send({success: true, message: "Saque efetuado com sucesso!"})
+})
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
+  const {customers} = req
+  const {date} = req.query
+  const dateFormat = new Date(date + " 00:00")
+
+  const statement = customers.statement.filter(customer => customer.create_at.toDateString() === new Date(dateFormat).toDateString())
+
+  if(statement.length <= 0) return res.status(400).send({success: false, message: "Extrato por data."})
+
+  return res.status(201).send({success: true, message: "Extrato por data.", statement})
+})
+
+app.put('/account', verifyIfExistsAccountCPF, (req, res) => {
+  const { name } = req.body
+  const { customer } = req
+
+  customer.name = name
+
+  return res.status(201).send({success: true, message: "atualização de dados.", customer})
+})
+
+app.get('/account', verifyIfExistsAccountCPF, (req,res) => {
+  const { customer } = req
+
+  return res.status(201).json( customer)
+})
+
+app.delete('/account', verifyIfExistsAccountCPF, (req,res) => {
+  const { customer } = req
+
+  customers.splice(customer, 1)
+
+  return res.status(200).json(customers)
+})
+
+app.get("/balance", verifyIfExistsAccountCPF, (req, res) => {
+  const {customer} = req
+
+  const balance = getBalace(customer.statement)
+
+  return res.status(201).json(balance)
 })
 
 app.listen(3333, console.log('server running'))
